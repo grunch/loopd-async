f7f89d23600389f3c9bc3a6e194a861d2dada318
Francisco Calderón <fjcalderon@gmail.com>
2019-04-09 14:49:39 -0300

    Add Stand-Alone REST API Server

---
+++ server.js
@@ -0,0 +1,46 @@
+const config = require('dotenv');
+const https = require('https');
+const { join } = require('path');
+const { readFileSync } = require('fs');
+const basicAuth = require('express-basic-auth');
+const bodyParser = require('body-parser');
+const compress = require('compression')();
+const cors = require('cors');
+const express = require('express');
+const logger = require('morgan');
+const {
+  getTerms,
+  loopOut,
+  loopOutQuote,
+} = require('./service');
+
+const {
+  TLS_DIR,
+  LOOPD_BASIC_AUTH_USER,
+  LOOPD_BASIC_AUTH_PASSWORD,
+} = process.env;
+const { log } = console;
+const app = express();
+const logFormat = ':method :url :status - :response-time ms - :user-agent';
+const port = process.env.PORT || 20010;
+const httpsPort = 20020;
+
+app.disable('x-powered-by');
+app.use(compress);
+app.use(cors());
+app.use(bodyParser.json());
+app.use(logger(logFormat));
+const users = {users:{}};
+const userName = LOOPD_BASIC_AUTH_USER || 'admin';
+users.users[userName] = LOOPD_BASIC_AUTH_PASSWORD;
+app.use(basicAuth(users));
+
+app.get('/v0/terms', getTerms);
+app.post('/v0/loop_out', loopOut);
+app.post('/v0/loop_out_quote', loopOutQuote);
+
+const [cert, key] = ['cert', 'key']
+  .map(extension => join(TLS_DIR, `tls.${extension}`))
+  .map(n => readFileSync(n, 'utf8'));
+const httpsServer = https.createServer({ cert, key }, app);
+httpsServer.listen(httpsPort, () => log(`Listening HTTPS on port: ${httpsPort}`));
---
+++ service/index.js
@@ -0,0 +1,9 @@
+const getTerms = require('./get_terms');
+const loopOut = require('./loop_out');
+const loopOutQuote = require('./loop_out_quote');
+
+module.exports = {
+  getTerms,
+  loopOut,
+  loopOutQuote,
+};
